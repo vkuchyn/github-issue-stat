c36a74adc80d0f18d8b22a5b08c4f531cff51c22
Viktor Kuchyn <kuchin.victor@gmail.com>
2016-05-21 22:40:04 +0300

    First quick implementation

--- src/main/java/juja/github/stat/GithubIssueSystem.java
+++ src/main/java/juja/github/stat/GithubIssueSystem.java
@@ -1,5 +1,7 @@
 package juja.github.stat;
 
+import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,6 +10,17 @@ import java.util.List;
 public final class GithubIssueSystem implements IssueSystem {
     @Override
     public List<TimeReport> fetchTimeReports() {
-        return null;
+        return Arrays.asList(
+            new TimeReport(
+                "220794586", "https://github.com/vkuchyn/github-issue-stat/issues/1#issuecomment-220794586",
+                "Some text before time report\n@JujaD time spent 10m\nSome text after report",
+                Duration.ofMinutes(10L)
+            ),
+            new TimeReport(
+                "220794605", "https://github.com/vkuchyn/github-issue-stat/issues/1#issuecomment-220794605",
+                "@JujaD time spend 1h 65m some text and more 20m",
+                Duration.ofMinutes(145L)
+            )
+        );
     }
 }

a04cf703c3fef058e4b1c2dfa92a72774a9dc1e5
Viktor Kuchyn <kuchin.victor@gmail.com>
2016-05-21 22:22:25 +0300

    IssueSystem integration test

---
+++ src/test/java/juja/github/stat/GithubIssueSystemIntegrationTest.java
@@ -0,0 +1,27 @@
+package juja.github.stat;
+
+import org.junit.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+
+/**
+ * @author viktor email kuchin.victor@gmail.com
+ */
+public class GithubIssueSystemIntegrationTest {
+    @Test
+    public void calculateTimeTrack() throws Exception {
+        List<TimeReport> reports = new GithubIssueSystem().fetchTimeReports();
+        assertThat(reports, hasSize(2));
+        assertThat(reports, hasItem(new TimeReport(
+            "220794586", "https://github.com/vkuchyn/github-issue-stat/issues/1#issuecomment-220794586",
+            "Some text before time report\n@JujaD time spent 10m\nSome text after report",
+            Duration.ofMinutes(10L)
+        )));
+
+    }
+}

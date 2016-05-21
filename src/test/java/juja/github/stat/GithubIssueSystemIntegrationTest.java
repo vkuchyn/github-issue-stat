7fa10ce60204772e37579f8a5ffb319fb59a329c
Viktor Kuchyn <kuchin.victor@gmail.com>
2016-05-21 22:27:55 +0300

    Test case

--- src/test/java/juja/github/stat/GithubIssueSystemIntegrationTest.java
+++ src/test/java/juja/github/stat/GithubIssueSystemIntegrationTest.java
@@ -22,6 +22,11 @@ public class GithubIssueSystemIntegrationTest {
             "Some text before time report\n@JujaD time spent 10m\nSome text after report",
             Duration.ofMinutes(10L)
         )));
+        assertThat(reports, hasItem(new TimeReport(
+            "220794605", "https://github.com/vkuchyn/github-issue-stat/issues/1#issuecomment-220794605",
+            "@JujaD time spend 1h 65m some text and more 20m",
+            Duration.ofMinutes(145L)
+        )));
 
     }
 }
